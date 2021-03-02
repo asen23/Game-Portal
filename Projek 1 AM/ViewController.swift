105bd3b85cb6822dc576c4f4263fad2488272881
Andersen <andersen@binus.edu>
2021-03-02 20:17:13 +0700

    Initial Commit

---
+++ Projek 1 AM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Projek 1 AM
+//
+//  Created by IT-Mobile-Dev on 02/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
